48b14d0484ebcf8c49f66200e7a654c66373aff1
Aaron <aaronpark87@gmail.com>
2015-04-17 11:56:59 -0400

    lab_edits

--- app/models/actor.rb
+++ app/models/actor.rb
@@ -1,3 +1,10 @@
-class Actors < ActiveRecord::Base
+class Actor < ActiveRecord::Base
 	has_and_belongs_to_many :movies
+
+	validates :name,
+	presence: true
+
+	validates :photo_url,
+	presence: true
+
 end
--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,3 +1,17 @@
-class Movies < ActiveRecord::Base
+class Movie < ActiveRecord::Base
 	has_and_belongs_to_many :actors
+
+	validates :title,
+	presence: true
+
+	validates :year,
+	presence: true
+
+	validates :plot,
+	presence: true
+	
+	validates :poster_url,
+	presence: true
+	
+
 end
